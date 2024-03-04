dc8f283331a6e7ce8cdf8c7b3cbcb129ac28fd04
deeboston <161074100+deeboston@users.noreply.github.com>
2024-03-04 07:52:54 -0500

    Add files via upload
    Create a python Clase animales 
    Nombre 
    Número de patas 
    Fecha de vacuna 
    Propietarios 
    
    Créate una función que imprima cada consoló los atributos mediante un mensaje personalizado

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from persona import Persona
+class Animal:
+  def __init__(self, nombre, num_patas, fecha_vacuna, propietarios):
+      self.nombre = nombre
+      self.num_patas = num_patas
+      self.fecha_vacuna = fecha_vacuna
+      self.propietarios = propietarios
 
-def main():
-    p = Persona("D'Andre", "Boston")
-    print(p.obtener_nombre())
+  def imprimir_nombre(self):
+      print(f"Nombre: {self.nombre}")
 
-if __name__ == "__main__":
-    main()
+  def imprimir_num_patas(self):
+      print(f"Número de patas: {self.num_patas}")
+
+  def imprimir_fecha_vacuna(self):
+      print(f"Fecha de vacuna: {self.fecha_vacuna}")
+
+  def imprimir_propietarios(self):
+      print(f"Propietarios: {', '.join(self.propietarios)}")
+
+# Producto
+perro = Animal("Fido", 4, "2023-06-15", ["D'Andre", "Boston"])
+perro.imprimir_nombre()
+perro.imprimir_num_patas()
+perro.imprimir_fecha_vacuna()
+perro.imprimir_propietarios()
